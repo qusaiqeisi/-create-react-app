455b2643704371ac8a75680661549a7de5d21d1e
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-06-23 17:28:07 +0300

    adding filter and form

--- src/App.js
+++ src/App.js
@@ -3,42 +3,11 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import {Container} from 'react-bootstrap';
-// import SelectedBeast  from './SelectedBeast ';
-// import Beast from './Data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
 class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     allData: Beast,
-  //     show: false,
-  //     selectedProps: {}
-     
-  //   }
-  // };
-
-  // displayAsModal = name => {
-  //   const selectedProps = Beast.find(beast => beast.title === name);
-  //   this.setState({
-  //     selectedProps,
-  //     show: true
-  //   });
-  // }
-  // handleModal = (prevProps) => {
-  //   this.setState({
-  //     show: false,
-  //     // selectedProps: prevProps
-  //   })
-  // }
-
-  // updateAllData = (allData) => {
-  //   this.setState({
-  //     allData
-  //   });
-  // }
+  
   render() {
     return (
       <div>
