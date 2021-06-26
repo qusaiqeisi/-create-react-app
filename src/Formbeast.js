f323c5654c6c6bde083ad7496dcb1e3576bdeb87
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-06-26 11:55:14 +0300

    code 4change done

--- src/Formbeast.js
+++ src/Formbeast.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
-import {Form} from 'react-bootstrap';
+// import React, { Component } from 'react'
+// import {Form} from 'react-bootstrap';
 
-export class Formbeast extends Component {
-  render() {
-    return (
-      <div id='form'>
-        <Form>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>HORNED BEAST ❤️ </Form.Label>
-            <Form.Control type="name" placeholder="Baby Rhino" />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlSelect1">
-            <Form.Label>View The Beasts By The Number Of Horns .</Form.Label>
-            <Form.Control onChange={this.filter} as="select">
-              <option vakue='1'>1</option>
-              <option vakue='1'>2</option>
-              <option vakue='1'>3</option>
-              <option vakue='1'>100</option>
-            </Form.Control>
-          </Form.Group>
-        </Form>
-      </div>
-    )
-  }
-}
+// export class Formbeast extends Component {
+//   render() {
+//     return (
+//       <div id='form'>
+//         <Form>
+//           <Form.Group controlId="exampleForm.ControlInput1">
+//             <Form.Label>HORNED BEAST ❤️ </Form.Label>
+//             <Form.Control type="name" placeholder="Baby Rhino" />
+//           </Form.Group>
+//           <Form.Group controlId="exampleForm.ControlSelect1">
+//             <Form.Label> .</Form.Label>
+//             <Form.Control onChange={this.filter} as="select">
+//               <option vakue='1'>1</option>
+//               <option vakue='1'>2</option>
+//               <option vakue='1'>3</option>
+//               <option vakue='1'>100</option>
+//             </Form.Control>
+//           </Form.Group>
+//         </Form>
+//       </div>
+//     )
+//   }
+// }
 
-export default Formbeast
+// export default Formbeast
